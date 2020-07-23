bea6c02358f11c6b00d483d2743331dd4e805b15
Subhrajitbw <39183617+Subhrajitbw@users.noreply.github.com>
2020-07-23 19:28:16 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to Susastho
                 </div>
 
                 <div class="links">
