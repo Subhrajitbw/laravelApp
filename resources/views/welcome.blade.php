43dad9b663c26595aa1ddd6c1b33c954e5925ff7
subhrajit bhowmik <subhrajitbhowmik80@gmail.com>
2020-07-23 20:22:58 +0000

    welcme edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    HI
+                   hi
                 </div>
 
                 <div class="links">
