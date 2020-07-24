e39cb51eb4cc212bdc2f7fdb1a2ce409ed30ee59
Subhrajitbw <subhrajitbhowmik@gmail.com>
2020-07-24 13:21:40 +0530

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Welcome to Susastho
+                   Hello World!
                 </div>
 
                 <div class="links">
