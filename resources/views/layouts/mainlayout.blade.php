35b8842004850c9e3940e9ed1916b46d47db20e0
Subhrajitbw <subhrajitbhowmik80@gmail.com>
2020-07-25 22:52:46 +0530

    Fixed inclusion issues

--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-   @include ('layouts.partials.head')
+   @include('layouts.partials.head')
 </head>
 <body>
     @yield('content')
