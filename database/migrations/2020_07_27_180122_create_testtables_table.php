304fd5eaf986ff155e802f62dd65f03fa9d1a039
Subhrajitbw <subhrajitbhowmik80@gmail.com>
2020-07-27 23:40:07 +0530

    db test

---
+++ database/migrations/2020_07_27_180122_create_testtables_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTesttablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name'); //varchar(255)
+            $table->string('last_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('testtables');
+    }
+}
