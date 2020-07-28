aec6165db260f06dd5c5c382e941f3fd45c18fbe
Subhrajitbw <subhrajitbhowmik80@gmail.com>
2020-07-28 21:02:17 +0530

    T

--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -6,12 +6,13 @@
 </head>
 <body>
     @include('layouts.partials.header')
+<?php
     if(DB::connection()->getDatabaseName())
 {
    echo "Connected to database ".DB::connection()->getDatabaseName();
 }
 $response->send();
-...
+?>
 </body>
 
 </html>
